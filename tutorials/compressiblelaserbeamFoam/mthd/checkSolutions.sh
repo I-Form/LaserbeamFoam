b66ccea95631f22a4e1ad84263f3b0d9bd7b2e19
roman-bialek <roman.bialek@postgrad.manchester.ac.uk>
2026-03-22 15:34:33 +0000

    Add diff script for solutions (WIP only, remove after debug)

---
+++ tutorials/compressiblelaserbeamFoam/mthd/checkSolutions.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Run check using diff and diffstat to see if solutions mutated
+folders=("0.001" "0.002" "0.003" "0.004" "0.005" "0.01" "0.02" "0.03" "0.04" "0.05" "0.06" "0.07" "0.08" "0.09")
+fields=("U" "T" "H" "p_rgh" "alphas" "divB")
+
+fpSource=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/laserbeamFoam/Plate2D_current
+#$PWD
+#$PWD / ./richter2dOrig 
+fpReference=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/laserbeamFoam/Plate2D_MTHD_original
+#Plate2D_orig
+#../richter2dAutoPtr/
+
+fpSource=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/compressiblelaserbeamFoam/mthd/richter2dAutoPtr
+#$PWD
+#$PWD / ./richter2dOrig 
+fpReference=../richter2D_MTHD_original
+
+
+
+fpReport=./check.log
+echo "Checking files" > ${fpReport}
+# myArray=("cat" "dog" "mouse" "frog"))
+echo "fpSource = ${fpSource}" >> ${fpReport}
+echo "fpReference = ${fpReference}" >> ${fpReport}
+
+for folder in ${folders[@]}; do
+    echo "(${folder})"
+    for field in ${fields[@]}; do
+        if [ -d "${fpSource}/${folder}" ]; then
+            echo "(${folder}, ${field})"
+            #^checks if directory exists
+
+            fileRef=${fpReference}/${folder}/${field}
+            fileSrc=${fpSource}/${folder}/${field}
+            
+            # Report the differences, if any
+            echo "${folder}/${field}:" >> ${fpReport}
+            diff -u ${fileRef} ${fileSrc} | diffstat 2>&1 | tee -a ${fpReport}
+        fi
+    done
+done
+
+echo "[checkSolutions.sh] Done."
