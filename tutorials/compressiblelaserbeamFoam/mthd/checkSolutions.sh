03f3be205eaf1bf2d9fe61faaca8b2087277842e
roman-bialek <roman.bialek@postgrad.manchester.ac.uk>
2026-03-23 09:52:46 +0000

    Use foamListTimes to get time directories

--- tutorials/compressiblelaserbeamFoam/mthd/checkSolutions.sh
+++ tutorials/compressiblelaserbeamFoam/mthd/checkSolutions.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
 # Run check using diff and diffstat to see if solutions mutated
-folders=("0.001" "0.002" "0.003" "0.004" "0.005" "0.01" "0.02" "0.03" "0.04" "0.05" "0.06" "0.07" "0.08" "0.09")
+# folders=("0.001" "0.002" "0.003" "0.004" "0.005" "0.01" "0.02" "0.03" "0.04" "0.05" "0.06" "0.07" "0.08" "0.09")
 fields=("U" "T" "H" "p_rgh" "alphas" "divB")
 
-fpSource=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/laserbeamFoam/Plate2D_current
+fpSource=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/laserbeamFoam/Plate2D
 #$PWD
 #$PWD / ./richter2dOrig 
 fpReference=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/laserbeamFoam/Plate2D_MTHD_original
 #Plate2D_orig
 #../richter2dAutoPtr/
 
-fpSource=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/compressiblelaserbeamFoam/mthd/richter2dAutoPtr
-#$PWD
-#$PWD / ./richter2dOrig 
-fpReference=../richter2D_MTHD_original
 
 
+# fpSource=~/Documents/openfoam/v2506/laserbeamfoam_dev_branches/fix_divzero_error/tutorials/compressiblelaserbeamFoam/mthd/richter2d_rerun2
+# #$PWD
+# #$PWD / ./richter2dOrig 
+# fpReference=../richter2D_MTHD_original
 
-fpReport=./check.log
+
+folders=$(foamListTimes -case ${fpSource}) # could pass `-time 0:``
+
+
+# Quick list
+## Richter 2D:
+# AutoPtr, Orig = Match
+# `==` old, Orig = Match
+# `==` Recompile, Orig = 
+#
+## Plate 2D:
+#  AutoPtr (T+U), orig = Match
+#  AutoPtr (T only), orig = Match
+#  With `==`
+
+fpReport=./solution\_check.log
 echo "Checking files" > ${fpReport}
 # myArray=("cat" "dog" "mouse" "frog"))
 echo "fpSource = ${fpSource}" >> ${fpReport}
@@ -40,5 +55,22 @@ for folder in ${folders[@]}; do
         fi
     done
 done
+
+# for folder in ${folders[@]}; do
+#     echo "(${folder})"
+#     for field in ${fields[@]}; do
+#         if [ -d "${fpSource}/${folder}" ]; then
+#             echo "(${folder}, ${field})"
+#             #^checks if directory exists
+
+#             fileRef=${fpReference}/${folder}/${field}
+#             fileSrc=${fpSource}/${folder}/${field}
+            
+#             # Report the differences, if any
+#             echo "${folder}/${field}:" >> ${fpReport}
+#             diff -u ${fileRef} ${fileSrc} >> ${fpReport}
+#         fi
+#     done
+# done
 
 echo "[checkSolutions.sh] Done."
